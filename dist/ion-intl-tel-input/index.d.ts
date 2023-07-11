bef394f856d45d51411ad5ee0b0527f3539dce91
Carlos R <carlosrodriguez@ugr.es>
2023-07-11 02:46:04 +0200

    Bumped to 2.0.1

--- dist/ion-intl-tel-input/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@crodriguezdominguez/ion-intl-tel-input" />
-export * from './public-api';
--- dist/ion-intl-tel-input/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/ion-intl-tel-input.directive';
-export * from './lib/ion-intl-tel-input/ion-intl-tel-input.component';
-export * from './lib/ion-intl-tel-input/ion-intl-tel-code.component';
-export * from './lib/ion-intl-tel-input.service';
-export * from './lib/models/ion-intl-tel-input.model';
-export * from './lib/ion-intl-tel-input.module';
