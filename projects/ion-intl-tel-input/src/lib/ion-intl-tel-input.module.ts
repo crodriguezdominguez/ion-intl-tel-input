c5924f3cec2c6161c7b11b1310b483908344a37e
Carlos R <carlosrodriguez@ugr.es>
2023-07-15 10:36:16 +0200

    Bumped to 2.0.5. Small fixes.

--- projects/ion-intl-tel-input/src/lib/ion-intl-tel-input.module.ts
+++ projects/ion-intl-tel-input/src/lib/ion-intl-tel-input.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { CountryPlaceholder } from './pipes/country-placeholder';
-import { IonIntlTelInputValidators, IonIntlTelInputValidatorDirective } from './ion-intl-tel-input.directive';
+import { IonIntlTelInputValidatorDirective } from './ion-intl-tel-input.directive';
 import { IonIntlTelInputComponent } from './ion-intl-tel-input/ion-intl-tel-input.component';
 import { IonIntlTelInputService } from './ion-intl-tel-input.service';
 import { IonIntTelCodeComponent } from './ion-intl-tel-input/ion-intl-tel-code.component';
@@ -20,6 +21,7 @@ import { IonIntTelCodeComponent } from './ion-intl-tel-input/ion-intl-tel-code.c
     CommonModule,
     FormsModule,
     IonicModule,
+    ScrollingModule
   ],
   exports: [
     IonIntlTelInputComponent,
